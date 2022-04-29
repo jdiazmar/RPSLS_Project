892fbbcd957a5fb5073b367c9f7f27c34d9f4ce7
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-29 14:57:47 -0400

    Updated Players Class File

--- players.py
+++ players.py
@@ -3,17 +3,29 @@ from select import select
 
 
 class Players:
-    def __init__(self):
-        self.players = []
-        self.select_player()
+    def __init__(self, name):
+        self.name = name
 
     def view_player(self):
-        pass 
+        print(f"You chose {self.name}!")
 
-    def select_player(self, name):
-        self.name = name 
+    def player_selection(self, name):
+        self.player_selection = name
+        self.view_player()
+        
 
-class SelectPlayers(Players):
-    def __init__(self):
-        super().__init__()
-        pass 
+player_one = Players("Rock")
+player_one.view_player()
+player_two =Players("Paper")
+player_two.view_player()
+player_three = Players("Scissors")
+player_three.view_player()
+player_four = Players("Lizard")
+player_four.view_player()
+player_five = Players("Spock")
+player_five.view_player()
+
+
+
+
+    
