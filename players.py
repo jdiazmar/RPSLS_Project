c600b0db019da456f302847a02feb98732fdcb14
Josmar Diaz <jdm.begreat28@gmail.com>
2022-04-30 17:18:52 -0400

    Updated Players File

--- players.py
+++ players.py
@@ -1,4 +1,5 @@
 from asyncio import selector_events
+from os import name
 from select import select
 
 
@@ -14,16 +15,29 @@ class Players:
         self.view_player()
         
 
-player_one = Players("Rock")
-player_one.view_player()
-player_two =Players("Paper")
-player_two.view_player()
-player_three = Players("Scissors")
-player_three.view_player()
-player_four = Players("Lizard")
-player_four.view_player()
-player_five = Players("Spock")
-player_five.view_player()
+class HumanPlayer:
+    def __init__(self) -> None:
+        super().__init__(name)
+
+    def view_player(self):
+        print(f"You chose {self.name}!")
+
+    def player_selection(self, name):
+        self.player_selection = name
+        self.view_player()
+        
+
+class ComputerPlayer:
+    def __init__(self) -> None:
+        super().__init__(name)
+
+    def view_player(self):
+        print(f"You chose {self.name}!")
+
+    def player_selection(self, name):
+        self.player_selection = name
+        self.view_player()
+
 
 
 
