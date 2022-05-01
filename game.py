d3486e0650f0ed1f56b16cec4f07012bd6d345de
Josmar Diaz <jdm.begreat28@gmail.com>
2022-05-01 18:47:43 -0400

    Update Run Game File

--- game.py
+++ game.py
@@ -11,12 +11,24 @@ class Game:
 
     def run_game(self) -> None:
         self.display_welcome()
+        single_player = self.single_player_or_naw()
+        if single_player == True:
+            self.player_two = AI()
+        while self.player_one.score < 2 and self.player_two.score < 2:
+            winner = self.who_won_gestures(self.player_one.choose_gesture('Player One'), self.player_two.choose_gesture('Player Two'))
+            if winner == "Player One has won":
+                self.player_one.score += 1
+            elif winner == "Player Two has won":
+                self.player_two.score += 1
+            print(f'{winner} this round! ')
+        self.display_winner(self.player_one.score, self.player_two.score)
 
     def display_welcome(self):
         print("Welcome to Rock, Paper, Scissors, Lizard, & Spock!")
         print("")
         print("Here are the rules for RPSLS: ")
         print("Rock crushes Scissors\Scissors cuts Paper\Paper covers Rock\Rock crushes Lizard\Lizard poisons Spock\Spock smashes Scissors\Scissors decapitates Lizard\Lizard eats Paper!")
+        print("")
         print("Let the games BEGIN!")
 
     def single_play_game(self):
@@ -32,6 +44,15 @@ class Game:
     def who_won_gestures(self, gesture_one, gesture_two):
         if gesture_one == gesture_two:
             return "It's a DRAW! Let's PLAY AGAIN!"
+
+        gestures = {'Rock' < ['Scissors' or 'Lizard'], 'Paper' < ['Rock' or 'Spock'], 'Scissors' < ['Paper' or 'Lizard'], 'Lizard' < ['Spock' or 'Paper'], 'Spock' < ['Scissors' or 'Rock']}
+
+        results = gestures[gesture_one].count(gesture_two)
+
+        if results == 1:
+            return 'Player One has won this round!'
+        else:
+            return 'Player Two has won this round!'
         
 
         
